a9087cbf11870c37eb87cb484a92d82191ab1e87
Igor <kalamejcevigor@gmail.com>
2024-08-08 02:23:19 +0300

    pop up menu

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function openPopup() {
-    document.getElementById("myPopup").style.display = "block";
-  }
+// function openPopup() {
+//     document.getElementById("myPopup").style.display = "block";
+//   }
 
-  function closePopup() {
-    document.getElementById("myPopup").style.display = "none";
-  }
+//   function closePopup() {
+//     document.getElementById("myPopup").style.display = "none";
+//   }
+
+const popup = document.getElementById('popup');
+const openPopupBtn = document.getElementById('open-popup');
+const closePopupBtn = document.querySelector('.close-popup');
+
+function openPopup() {
+    popup.style.display = 'block';
+}
+
+function closePopup() {
+    popup.style.display = 'none';
+}
+
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
